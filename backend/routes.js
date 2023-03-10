6baf5cea9ab39d0490cf421afe89a5ddbd80163d
Yash <yashdahekar92@gmail.com>
2023-03-10 22:39:27 +0530

    get api

--- backend/routes.js
+++ backend/routes.js
@@ -4,7 +4,11 @@ app.use(express.json());
 const { connection } = require('./connector');
 const router = express.Router();
 
-
+//Creating get api
+router.get('/api/booking', async (req, res) => {
+    const data = await connection.find().exec();
+    return res.status(200).send(data[data.length - 1]);
+  });
 
 //Creating delete api
 router.delete('/api/booking', async (req, res) => {
