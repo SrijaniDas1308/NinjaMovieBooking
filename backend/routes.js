48aba4abdb287ef9e726197df54cbe897a77f401
Srijani Das <srijanidas1@gmail.com>
2023-03-10 22:42:06 +0530

    created post api

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,29 @@ app.use(express.json());
 const { connection } = require('./connector');
 const router = express.Router();
 
+//Creating post api
+router.post("/api/booking", async (req, res) => {
+  const { movie, slot, seats } = req.body;
+
+  if (!movie) {
+    return res.status(422).send('Please select a Movie');
+  }
+  if (!slot) {
+    return res.status(422).send('Please select a Time Slot');
+  }
+
+  if (seats.A1 || seats.A2 || seats.A3 || seats.A4 || seats.D1 || seats.D2) {
+    await connection(req.body).save((err, info) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(info);
+    });
+  } else {
+    return res.status(422).send('Please select a Seat!');
+  }
+});
+
 //Creating get api
 router.get('/api/booking', async (req, res) => {
     const data = await connection.find().exec();
