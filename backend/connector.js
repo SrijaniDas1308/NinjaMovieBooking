f571175b1ed4c69ee0b9824a6fdd9304be051daa
Srijani Das <srijanidas1@gmail.com>
2023-03-12 15:56:47 +0530

    Test

--- backend/connector.js
+++ backend/connector.js
@@ -1,6 +1,7 @@
+const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const { bookMovieSchema } = require('./schema');
-const MongoUrl="mongodb://localhost:27017/ninjamovies";
+const MongoUrl= "mongodb+srv://Srijani:ninja1234@cluster0.9xu32xd.mongodb.net/ninjamovies";
 
 mongoose.connect(MongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { 
